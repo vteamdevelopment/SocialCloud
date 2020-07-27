8651c9c3cdc4f25c28f9e1c00e4f6587e48f5199
Vikash Pandey <vikash.pandey1@viacom18.comgit>
2020-07-27 10:13:32 +0530

    Addition of Date Utils class.

---
+++ app/src/main/java/com/vteam/testdemo/common/DateUtils.kt
@@ -0,0 +1,13 @@
+package com.vteam.testdemo.common
+
+import java.util.*
+
+
+object DateUtils {
+    fun getCurrentTimeInUTC(){
+
+        val currentDate = Date()
+        println("Current date: $currentDate")
+
+    }
+}
