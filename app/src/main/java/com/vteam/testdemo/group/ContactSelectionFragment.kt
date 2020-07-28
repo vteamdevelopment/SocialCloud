ff307d764905278f6faa2b9c4c9ce1563fb65238
Vikash Pandey <vikash.pandey1@viacom18.comgit>
2020-07-28 19:23:23 +0530

    Migration of ContactSelectionFragment to kotlin.

---
+++ app/src/main/java/com/vteam/testdemo/group/ContactSelectionFragment.kt
@@ -0,0 +1,164 @@
+package com.vteam.testdemo.group
+
+import android.app.Activity
+import android.content.Intent
+import android.os.Bundle
+import android.text.TextUtils
+import android.util.Log
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.DividerItemDecoration
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import com.firebase.ui.database.FirebaseRecyclerAdapter
+import com.firebase.ui.database.FirebaseRecyclerOptions
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.database.Query
+import com.google.firebase.storage.FirebaseStorage
+import com.vteam.testdemo.R
+import com.vteam.testdemo.chat.ChatActivity
+import com.vteam.testdemo.common.Constants
+import com.vteam.testdemo.landing.model.Users
+import de.hdodenhof.circleimageview.CircleImageView
+
+class ContactSelectionFragment : Fragment() {
+    private var mContactsView: View? = null
+    private var mContactsList: RecyclerView? = null
+    private var mUsersRef: DatabaseReference? = null
+    private var mAuth: FirebaseAuth? = null
+    private val mCurrentUserID: String? = null
+    private val senderUserID: String? = null
+    private var adapter: FirebaseRecyclerAdapter<Users, ContactsViewHolder>? =
+        null
+    private var query: Query? = null
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        mContactsView = inflater.inflate(R.layout.fragment_contacts, container, false)
+        mContactsList = mContactsView?.findViewById<View>(R.id.group_list) as RecyclerView
+        val layoutManager = LinearLayoutManager(context)
+        mContactsList!!.layoutManager = layoutManager
+        val mDividerItemDecoration = DividerItemDecoration(
+            mContactsList!!.context,
+            layoutManager.orientation
+        )
+        mContactsList!!.addItemDecoration(mDividerItemDecoration)
+        mAuth = FirebaseAuth.getInstance()
+        mUsersRef = FirebaseDatabase.getInstance().reference
+            .child(Constants.NODES.USER_NODE)
+        query = mUsersRef!!.limitToLast(50)
+        return mContactsView
+    }
+
+    override fun onStart() {
+        super.onStart()
+        val options = FirebaseRecyclerOptions.Builder<Users>()
+            .setQuery(query!!, Users::class.java)
+            .build()
+
+        adapter = object : FirebaseRecyclerAdapter<Users, ContactsViewHolder>(options) {
+            override fun onBindViewHolder(
+                holder: ContactsViewHolder,
+                position: Int,
+                model: Users
+            ) {
+                val visitorUserId = getRef(position).key
+                val name = model.name
+                val profileImage = model.image
+                holder.userName.text = name
+                holder.userStatus.text = model.status
+                if (model.userStatus != null && model.userStatus!!.status != null) {
+                    if (model.userStatus!!.status == Constants.USER_STATE.OFFLINE) {
+                        holder.onlineIcon.background =
+                            context!!.getDrawable(R.drawable.live_icon_grey)
+                    } else if (model.userStatus!!.status == Constants.USER_STATE.ONLINE) {
+                        holder.onlineIcon.background =
+                            context!!.getDrawable(R.drawable.live_icon_green)
+                    }
+                    holder.lastSeenTime.text = model.userStatus!!.time
+                }
+                if (profileImage != null && !profileImage.isEmpty()) {
+                    val reference = FirebaseStorage.getInstance().reference
+                    reference.child(profileImage).downloadUrl
+                        .addOnSuccessListener { uri -> // Got the download URL for 'users/me/profile.png'
+                            Log.d("URL", "" + uri)
+                            val activity: Activity? = activity
+                            if (activity != null) {
+                                Glide.with(activity)
+                                    .load(uri)
+                                    .into(holder.profileImage)
+                            }
+                        }.addOnFailureListener {
+                            // Handle any errors
+                        }
+                }
+                if (!TextUtils.isEmpty(profileImage)) {
+                    Glide.with(activity!!)
+                        .load(profileImage)
+                        .into(holder.profileImage)
+                }
+                holder.itemView.setOnClickListener {
+                    SendChatRequest(
+                        visitorUserId,
+                        name,
+                        profileImage
+                    )
+                }
+            }
+
+            override fun onCreateViewHolder(
+                viewGroup: ViewGroup,
+                i: Int
+            ): ContactsViewHolder {
+                val view = LayoutInflater.from(viewGroup.context)
+                    .inflate(R.layout.contact_item_layout, viewGroup, false)
+                return ContactsViewHolder(view)
+            }
+        }
+        mContactsList!!.adapter = adapter
+        adapter?.startListening()
+    }
+
+    private fun SendChatRequest(
+        receiverUserID: String?,
+        refName: String?,
+        imageUrl: String?
+    ) {
+        val chatIntent = Intent(context, ChatActivity::class.java)
+        chatIntent.putExtra("visit_user_id", receiverUserID)
+        chatIntent.putExtra("visit_user_name", refName)
+        chatIntent.putExtra("visit_image", imageUrl)
+        startActivity(chatIntent)
+    }
+
+    class ContactsViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        val userName: TextView
+        val lastSeenTime: TextView
+        val profileImage: CircleImageView
+        val userStatus: TextView
+        val onlineIcon: ImageView
+
+        init {
+            userName = itemView.findViewById(R.id.user_profile_name)
+            lastSeenTime = itemView.findViewById(R.id.text_last_message_date)
+            userStatus = itemView.findViewById(R.id.text_status)
+            profileImage = itemView.findViewById(R.id.users_profile_image)
+            onlineIcon =
+                itemView.findViewById<View>(R.id.users_status_icon) as ImageView
+        }
+    }
+
+    companion object {
+        fun newInstance(): ContactSelectionFragment {
+            return ContactSelectionFragment()
+        }
+    }
+}
